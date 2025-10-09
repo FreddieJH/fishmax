d989f925c221af256fc51e9caaf99cadea9550b1
Freddie Heather <freddie.heather@utas.edu.au>
2025-10-09 14:40:41 +1100

    traceplot loop over named list

--- R/plot_traceplot.R
+++ R/plot_traceplot.R
@@ -10,22 +10,29 @@
 #' @importFrom tidyr pivot_longer
 plot_traceplot <- function(fit) {
   posterior_samples <- get_posterior(fit)
-  n_params <- ncol(posterior_samples) - 3 # chain, iteration and draw cols
 
-  posterior_samples |>
-    tidyr::pivot_longer(cols = -c(.chain, .iteration, .draw)) |>
-    ggplot2::ggplot(ggplot2::aes(
-      .iteration,
-      value,
-      colour = as.factor(.chain),
-      group = .chain
-    )) +
-    ggplot2::geom_path(alpha = 0.4) +
-    ggplot2::facet_wrap(
-      ~name,
-      scales = "free",
-      ncol = ceiling(sqrt(n_params))
-    ) +
-    ggplot2::theme_classic(20) +
-    ggplot2::theme(legend.position = "none")
+  purrr::map(
+    names(posterior_samples)[names(posterior_samples) != "maxima"],
+    function(model_name) {
+      n_params <- ncol(posterior_samples[[model_name]]) - 3
+
+      posterior_samples[[model_name]] |>
+        tidyr::pivot_longer(cols = -c(.chain, .iteration, .draw)) |>
+        ggplot2::ggplot(ggplot2::aes(
+          .iteration,
+          value,
+          colour = as.factor(.chain),
+          group = .chain
+        )) +
+        ggplot2::geom_path(alpha = 0.4) +
+        ggplot2::facet_wrap(
+          ~name,
+          scales = "free",
+          ncol = ceiling(sqrt(n_params))
+        ) +
+        ggplot2::labs(title = model_name) +
+        ggplot2::theme_classic(20) +
+        ggplot2::theme(legend.position = "none")
+    }
+  )
 }
